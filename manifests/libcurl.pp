3d8fd643aeb9ab1f13aef82e1b5c7cb62421421d
James Armes <jamesiarmes@gmail.com>
2014-06-18 11:48:51 -0400

    [#73091300] Updated to traceview terminology.

--- manifests/libcurl.pp
+++ manifests/libcurl.pp
@@ -1,12 +1,12 @@
-class tracelytics::libcurl {
+class traceview::libcurl {
 
-  include tracelytics
+  include traceview
 
-  $tracelytics_libcurl_version = hiera('tracelytics_libcurl_version', '7.22.0-3ubuntu4.1+tracelytics2')
+  $traceview_libcurl_version = hiera('traceview_libcurl_version', '7.22.0-3ubuntu4.1+tracelytics2')
 
   package { "libcurl3":
-    ensure  => $tracelytics_libcurl_version,
-    require => [ Package["liboboe0"], Apt::Source["tracelytics"] ],
+    ensure  => $traceview_libcurl_version,
+    require => [ Package["liboboe0"], Apt::Source["traceview"] ],
   }
 
 }
